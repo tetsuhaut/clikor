a3e56640f8a33aed266747bc50b1e4f67ba19c84
simon <johndoe@example.com>
2023-08-22 23:35:10 +0200

    remove warnings

--- src/main/cpp/system.cpp
+++ src/main/cpp/system.cpp
@@ -2,44 +2,42 @@ module;
 
 #include <format>
 #include <Windows.h>
-#include <stdio.h>
+#include <iostream>
 
 export module system;
 
 export namespace os {
-LRESULT CALLBACK MyMouseCallback(int nCode, WPARAM wParam, LPARAM lParam);
-
+/**
+ * Encapsulates the mouse hook installation/uninstallation in the Windows OS.
+ */
 class [[nodiscard]] MouseEventListener final {
 private:
   HHOOK m_mouseHook; // handle to the mouse hook
-  MSG m_msg; // struct with information about all messages in our queue
-
-  friend LRESULT CALLBACK MyMouseCallback(int nCode, WPARAM wParam, LPARAM lParam);
+  static LRESULT CALLBACK mouseCallback(int nCode, WPARAM wParam, LPARAM lParam) noexcept;
 public:
   MouseEventListener();
   ~MouseEventListener();
-
-  // function to "deal" with our messages
-  int Messsages();
 }; // class MouseEventListener
 } // namespace system
 
 module : private;
 
-os::MouseEventListener::MouseEventListener()
-  : m_mouseHook(nullptr),
-    m_msg() {
-  /*
-    SetWindowHookEx(
-    WM_MOUSE_LL = mouse low level mouseHook type,
-    MyMouseCallback = our callback function that will deal with system messages about mouse
-    NULL, 0);
+[[nodiscard]] static std::string getLastErrorMessageFromOS() {
+  char err[MAX_PATH + 1] { '\0' };
+  FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, GetLastError(),
+                MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), &err[0], MAX_PATH,
+                nullptr);
+  return &err[0];
+}
 
-    c++ note: we can check the return SetWindowsHookEx like this because:
-    If it return NULL, a NULL value is 0 and 0 is false.
-    */
-  if (nullptr == (m_mouseHook = SetWindowsHookEx(WH_MOUSE_LL, MyMouseCallback, NULL, 0))) {
-    printf_s("Error: %lu \n", GetLastError());
+/**
+ * @see https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwindowshookexa
+ */
+os::MouseEventListener::MouseEventListener()
+  : m_mouseHook(nullptr) {
+  if (nullptr == (m_mouseHook = SetWindowsHookEx(WH_MOUSE_LL, mouseCallback, nullptr, 0))) {
+    std::cerr << getLastErrorMessageFromOS() << '\n';
+    std::cerr << std::format("Error code {} \n", GetLastError());
   }
 }
 
@@ -47,42 +45,28 @@ os::MouseEventListener::~MouseEventListener() {
   UnhookWindowsHookEx(m_mouseHook);
 }
 
-// function to "deal" with our messages
-int os::MouseEventListener::Messsages() {
-  // while we do not close our application
-  while (WM_QUIT != m_msg.message) {
-    if (GetMessage(&m_msg, NULL, 0, 0/*, PM_REMOVE*/)) {
-      TranslateMessage(&m_msg);
-      DispatchMessage(&m_msg);
-    }
+/**
+ * @see https://learn.microsoft.com/en-us/windows/win32/winmsg/lowlevelmouseproc
+ */
+LRESULT CALLBACK os::MouseEventListener::mouseCallback(int nCode, WPARAM event, LPARAM data) noexcept {
+  try {
+    if (HC_ACTION == nCode) {
+      const auto& mouseData { *reinterpret_cast<MSLLHOOKSTRUCT*>(data) };
 
-    Sleep(1);
+      switch (event) {
+        case WM_LBUTTONDOWN: {
+          std::cout << std::format("LEFT CLICK DOWN x={} y={}\n", mouseData.pt.x, mouseData.pt.y);
+        } break;
+
+        case WM_LBUTTONUP: {
+          std::cout << std::format("LEFT CLICK UP x={} y={}\n", mouseData.pt.x, mouseData.pt.y);
+        } break;
+      }
+    }
+  } catch (...) {
+    std::cerr << "Error in mouseCallback!!!\n";
+    std::cerr << getLastErrorMessageFromOS() << '\n';
   }
 
-  UnhookWindowsHookEx(m_mouseHook); // if we close, let's uninstall our mouseHook
-  return (int)m_msg.wParam; // return the messages
-}
-
-LRESULT CALLBACK os::MyMouseCallback(int nCode, WPARAM wParam, LPARAM lParam) {
-  if (nCode == 0) {
-    auto* pMouseStruct = (MSLLHOOKSTRUCT*)lParam; // WH_MOUSE_LL struct
-    switch (wParam) {
-      case WM_LBUTTONDOWN: {
-        printf_s("%s", std::format("LEFT CLICK DOWN x={} y={}\n", pMouseStruct->pt.x, pMouseStruct->pt.y).c_str());
-      } break;
-
-      case WM_LBUTTONUP: {
-        printf_s("%s", std::format("LEFT CLICK UP x={} y={}\n", pMouseStruct->pt.x, pMouseStruct->pt.y).c_str());
-      } break;
-    }
-  }
-
-  /*
-  Every time that the nCode is less than 0 we need to CallNextHookEx:
-  -> Pass to the next mouseHook
-       MSDN: Calling CallNextHookEx is optional, but it is highly recommended;
-       otherwise, other applications that have installed hooks will not receive mouseHook notifications and may behave incorrectly as a result.
-  */
-  HHOOK ignored { nullptr };
-  return CallNextHookEx(ignored, nCode, wParam, lParam);
+  return CallNextHookEx(nullptr, nCode, event, data);
 }
